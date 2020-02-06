1b9fe5b76086758d4afdfbb2d6bc810f22ba2ba1
Abhishekparas <54927166+Abhishekparas@users.noreply.github.com>
2020-02-06 14:00:00 +0530

    Add files via upload
    sum of elements of the array is odd

--- main.cpp
+++ main.cpp
@@ -1,27 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int main() {
+    int testcases;
+    cin>>testcases;
     int n;
-    cin>>n;
-    int sum =0;
     int arr[100];
-    int leftmax[100];
-    int rightmax[100];
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    for(int i=0; i<testcases; i++){
+        cin>>n;
+        int sum =0;
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+            sum  = sum + arr[i];
+        }
+        if(sum&1){
+        cout<<"YES"<<endl;
+        }
+        else{
+        cout<<"NO"<<endl;
+        }
     }
-    leftmax[0] = arr[0] ;
-    rightmax[n-1]= arr[n-1];
 
-    for(int i=1; i<n; i++){
-        leftmax[i] = max(arr[i], leftmax[i-1]);
-    }
-    for(int i=n-2; i>=0; i--){
-        rightmax[i] = max(arr[i], rightmax[i+1]);
-    }
-    for(int i=0; i<n;i++){
-        sum  = sum + (min(leftmax[i],rightmax[i]) - arr[i]);
-    }
-    cout<<sum;
+
     return 0;
 }
